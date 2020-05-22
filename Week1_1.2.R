e70b0ecbc627630de6213fb80538943e3d28e02c
thomasvn <thomasnguyen96@gmail.com>
2020-05-22 15:51:28 -0700

    First coding challenge from Coursera

---
+++ Week1_1.2.R
@@ -0,0 +1,25 @@
+# Code Challenge: Implement PatternCount (reproduced below).
+# Input: Strings Text and Pattern.
+# Output: Count(Text, Pattern).
+
+# PatternCount(Text, Pattern)
+#   count ← 0
+#   for i ← 0 to |Text| − |Pattern|
+#     if Text(i, |Pattern|) = Pattern
+#       count ← count + 1
+#     return count
+
+library(stringr)
+
+# Read in data, and parse for the text and pattern
+data_file <- "data/week1_1.2.txt"
+data <- read.delim(data_file, header=FALSE, sep="\n")
+data <- unlist(data)
+data_text <- data[1]
+data_pattern <- data[2]
+
+# Count the number of times the pattern shows up in the text
+# Regex performs lookahead assertions
+data_pattern <- str_c("(?=", data_pattern, ")")
+str_count(data_text, pattern=data_pattern)
+
